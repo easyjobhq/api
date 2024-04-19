8715778770a02055826784746601493d994a3a8b
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-04-18 21:52:54 -0500

    feat: making the service of the client of easy_job

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,26 +1,87 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Client } from './entities/client.entity';
+import { Repository } from 'typeorm';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { isUUID } from 'class-validator';
 
 @Injectable()
 export class ClientsService {
-  create(createClientDto: CreateClientDto) {
-    return 'This action adds a new client';
+
+
+  private readonly logger = new Logger('ClientsService');
+
+  constructor(
+    @InjectRepository(Client)
+    private readonly clientRespository: Repository<Client>
+  ) {}
+
+  async create(createClientDto: CreateClientDto) {
+    const client =  this.clientRespository.create(createClientDto);
+
+    await this.clientRespository.save(client);
+
+    return client;
   }
 
-  findAll() {
-    return `This action returns all clients`;
+  findAll( paginationDto: PaginationDto ) {
+    const {limit = 10, offset= 0} = paginationDto;
+
+    return this.clientRespository.find({
+      take: limit, 
+      skip: offset,
+    })
+
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} client`;
+  async findOne(id_client: string) {
+
+    let client: Client;
+
+    if(isUUID(id_client)){
+      client = await this.clientRespository.findOneBy({id: id_client});
+    }
+
+    if(!client){
+      throw new NotFoundException(`Client with ${id_client} not found`)
+    }
+
+    return client;
   }
 
-  update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
+  async update(id: string, updateClientDto: UpdateClientDto) {
+    const client = await this.clientRespository.preload({
+      id: id,
+      ...updateClientDto
+    });
+
+    if ( !client ) throw new NotFoundException(`Brand with id: ${ id } not found`);
+
+    try {
+      await this.clientRespository.save( client );
+      return client;
+      
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} client`;
+  async remove(id: string) {
+    const client = await this.findOne(id);
+    await this.clientRespository.remove(client);
   }
+
+  private handleDBExceptions( error: any ) {
+
+    if ( error.code === '23505' )
+      throw new BadRequestException(error.detail);
+    
+    this.logger.error(error)
+    // console.log(error)
+    throw new InternalServerErrorException('Unexpected error, check server logs');
+
+  }
+
 }
