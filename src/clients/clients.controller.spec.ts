d859264366c9a2616857fcccbcdb666cb660d1a7
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-04-22 21:52:06 -0500

    test: finishing the test of the clients controller

--- src/clients/clients.controller.spec.ts
+++ src/clients/clients.controller.spec.ts
@@ -5,10 +5,52 @@ import { ClientsService } from './clients.service';
 describe('ClientsController', () => {
   let controller: ClientsController;
 
+  const clients = [{
+      id: "1",
+      name: "Maria",
+      last_name: "Gomez",
+      email: "maria@gmail.com",
+      photo_url: "wwww.fotico.com/maria",
+      phone_number: "+57 3109876543",
+      password: "Maria123*"
+    },
+    {
+      id: "2",
+      name: "Pedro",
+      last_name: "Lopez",
+      email: "pedro@gmail.com",
+      photo_url: "wwww.fotico.com/pedro",
+      phone_number: "+57 3158765432",
+      password: "Pedro456*"
+    },
+    {
+      id: "3",
+      name: "Laura",
+      last_name: "Martinez",
+      email: "laura@gmail.com",
+      photo_url: "wwww.fotico.com/laura",
+      phone_number: "+57 3187654321",
+      password: "Laura789*"
+  }]
+
   const mockClientsService = {
+
     create: jest.fn((dto) => ({id: Math.floor(Math.random() * 100), ...dto})), 
     update: jest.fn((id, dto) => ({id, ...dto})), 
-    remove: jest.fn((id) => ({}) )
+    remove: jest.fn((id) => ({}) ),
+    findOne: jest.fn((id) =>(
+      {
+        id: "1",
+        name: "Maria",
+        last_name: "Gomez",
+        email: "maria@gmail.com",
+        photo_url: "wwww.fotico.com/maria",
+        phone_number: "+57 3109876543",
+        password: "Maria123*"
+      }
+    ) ),
+    findAll: jest.fn(() => (clients))
+    
   }
   
   beforeEach(async () => {
@@ -80,6 +122,22 @@ describe('ClientsController', () => {
 
 
   it('should find a client', () =>  {
+    const dto = {
+      id: "1",
+      name: "Maria",
+      last_name: "Gomez",
+      email: "maria@gmail.com",
+      photo_url: "wwww.fotico.com/maria",
+      phone_number: "+57 3109876543",
+      password: "Maria123*"
+  }   
+    expect(controller.findOne('1')).toEqual(dto);
+    expect(mockClientsService.findOne).toHaveBeenCalledWith('1');
+    expect(mockClientsService.findOne).toHaveBeenCalledTimes(1);
+  })
 
+  it('should retrieve all', () => {
+    expect(controller.findAll()).toEqual(clients);
+    expect(mockClientsService.findAll).toHaveBeenCalledTimes(1);
   })
 });
