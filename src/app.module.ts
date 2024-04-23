a3020a70ffd6e18a5ad394dea012f70708c86dce
Mateo Silva <1000035385@u.icesi.edu.co>
2024-04-23 10:35:56 -0500

    prove dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,6 +24,7 @@ import { SeedModule } from './seed/seed.module';
       autoLoadEntities: true, 
       synchronize: true 
     }),
+
     ClientsModule, 
     ProfessionalsModule, GeneralResourcesModule, ClientProfessionalEntitiesModule, AuthModule, SeedModule],
   controllers: [AppController],
