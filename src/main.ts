ca18f73cc65ee8ef3a894fece467ad57d99aa869
Mateo Silva Lasso <102816058+MateoSilvaLasso@users.noreply.github.com>
2024-05-22 12:17:17 -0500

    AddCors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   // Enable CORS with specific options
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from this origin
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // Allowed headers
+    credentials: true, // Allow credentials (cookies, authorization headers)
+  });
+
+  
   await app.listen(3000);
 }
 bootstrap();
