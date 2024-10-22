b7e92cc89faaf05efb86aaa19a176c2531dd5455
Mateo Silva <1000035385@u.icesi.edu.co>
2024-10-22 14:57:42 -0500

    feat_client_profile

--- src/clients/controllers/clients.controller.ts
+++ src/clients/controllers/clients.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseInterceptors, UploadedFile, UseGuards } from '@nestjs/common';
 import { ClientsService } from '../clients.service';
 import { CreateClientDto } from '../dto/create-client.dto';
 import { UpdateClientDto } from '../dto/update-client.dto';
 import { PaginationDto } from '../../common/dtos/pagination.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('clients')
 export class ClientsController {
@@ -30,8 +31,10 @@ export class ClientsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateClientDto: UpdateClientDto) {
-    return this.clientsService.update(id, updateClientDto);
+  @UseInterceptors(FileInterceptor('client_image'))
+  @UseGuards(AuthGuard())
+  update(@Param('id') id: string,@UploadedFile() clientPhoto: Express.Multer.File, @Body() updateClientDto: UpdateClientDto) {
+    return this.clientsService.update(id, updateClientDto, clientPhoto);
   }
 
   @Delete(':id')
