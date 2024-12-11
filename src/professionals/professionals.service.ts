56c8e48ead94de923afedd26a99b00e536bc3389
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-12-10 23:05:38 -0500

    style: changing the query for specialities and cities

--- src/professionals/professionals.service.ts
+++ src/professionals/professionals.service.ts
@@ -1,15 +1,15 @@
-import {BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException} from '@nestjs/common';
-import {CreateProfessionalDto} from './dto/create-professional.dto';
-import {UpdateProfessionalDto} from './dto/update-professional.dto';
-import {InjectRepository} from '@nestjs/typeorm';
-import {Professional} from './entities/professional.entity';
-import {Repository} from 'typeorm';
-import {isUUID} from 'class-validator';
-import {Service} from './entities/service.entity';
-import {ServiceService} from './service.service';
-import {Speciality} from './entities/speciality.entity';
-import {SpecialityService} from './speciality.service';
-import {City} from "../general_resources/entities/city.entity";
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { CreateProfessionalDto } from './dto/create-professional.dto';
+import { UpdateProfessionalDto } from './dto/update-professional.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Professional } from './entities/professional.entity';
+import { Repository } from 'typeorm';
+import { isUUID } from 'class-validator';
+import { Service } from './entities/service.entity';
+import { ServiceService } from './service.service';
+import { Speciality } from './entities/speciality.entity';
+import { SpecialityService } from './speciality.service';
+import { City } from "../general_resources/entities/city.entity";
 import { LanguageService } from '../general_resources/services/language.service';
 import { CityService } from '../general_resources/services/city.service';
 import { Appointment } from '../client_professional_entities/entities/appointment.entity';
@@ -22,7 +22,7 @@ import { Review } from '../client_professional_entities/entities/review.entity';
 export class ProfessionalsService {
 
   private readonly logger = new Logger('ProfessionalService');
-  
+
 
   constructor(
     @InjectRepository(Professional)
@@ -40,95 +40,98 @@ export class ProfessionalsService {
     private readonly languageService: LanguageService,
     private readonly cityService: CityService,
     private s3Service: S3Service
-  ) {}
+  ) { }
 
-    async create(createProfessionalDto: CreateProfessionalDto, professionalPhoto: Express.Multer.File) {
-      
-      //Uploading the file to S3
-      const photoUrl = await this.s3Service.uploadFile(professionalPhoto, professionalPhoto.originalname);
+  async create(createProfessionalDto: CreateProfessionalDto, professionalPhoto: Express.Multer.File) {
 
-      const professional =  this.professionalRepository.create(createProfessionalDto);
-      professional.photo_url = photoUrl 
-      
-      const service = await this.serviceService.findOne(createProfessionalDto.service_id);
-      const language = await this.languageService.findOne(createProfessionalDto.language_id);
-      const city = await this.cityService.findOne(createProfessionalDto.city_id);
-      const speciality = await this.specialityService.findOne(createProfessionalDto.speciality_id);
+    //Uploading the file to S3
+    const photoUrl = await this.s3Service.uploadFile(professionalPhoto, professionalPhoto.originalname);
 
-      professional.services = professional.services || [];
-      professional.languages = professional.languages || [];
-      professional.cities = professional.cities || [];
-      professional.specialities = professional.specialities || [];
+    const professional = this.professionalRepository.create(createProfessionalDto);
+    professional.photo_url = photoUrl
 
-      if(service) professional.services.push(service);
-      if(language) professional.languages.push(language);
-      if(city) professional.cities.push(city);
-      if(speciality) professional.specialities.push(speciality);
-      professional.score = "0"
-      
-      await this.professionalRepository.save(professional);
+    const service = await this.serviceService.findOne(createProfessionalDto.service_id);
+    const language = await this.languageService.findOne(createProfessionalDto.language_id);
+    const city = await this.cityService.findOne(createProfessionalDto.city_id);
+    const speciality = await this.specialityService.findOne(createProfessionalDto.speciality_id);
 
-      return professional;
+    professional.services = professional.services || [];
+    professional.languages = professional.languages || [];
+    professional.cities = professional.cities || [];
+    professional.specialities = professional.specialities || [];
+
+    if (service) professional.services.push(service);
+    if (language) professional.languages.push(language);
+    if (city) professional.cities.push(city);
+    if (speciality) professional.specialities.push(speciality);
+    professional.score = "0"
+
+    await this.professionalRepository.save(professional);
+
+    return professional;
+  }
+
+  async saveProfessional(profesional: Professional) {
+    try {
+      await this.professionalRepository.save(profesional);
+      return profesional;
+    } catch (error) {
+      this.handleDBExceptions(error);
     }
+  }
 
-    async saveProfessional(profesional: Professional) {
-      try {
-        await this.professionalRepository.save(profesional);
-        return profesional;
-      } catch (error) {
-        this.handleDBExceptions(error);
-      }
-    }
+  async findProfessionalsBySpeciality(speciality_name: string) {
+    const professionals = await this.professionalRepository.find({
+      where: {
+        specialities: {
+          speciality_name: speciality_name
+        }
+      },
+      relations: ['specialities', 'places']
+    });
 
-    async findProfessionalsBySpeciality(speciality_name: string) {
-      const professionals = await this.professionalRepository.find({
-        where: {
-          specialities: {
-            speciality_name: speciality_name
-          }
+    return professionals;
+
+  }
+
+  async findAll(limit: number, offset: number): Promise<[Professional[], number]> {
+    const [results, total] = await this.professionalRepository.findAndCount({
+      skip: offset,
+      take: limit,
+      relations: ['cities', 'specialities', 'services', 'places']
+    });
+    return [results, total];
+  }
+
+
+  async findByCityAndSpeciality(cityName: string, specialityName: string, limit: number, offset: number): Promise<[Professional[], number]> {
+    const [results, total] = await this.professionalRepository.findAndCount({
+      where: {
+        cities: {
+          city_name: cityName
         },
-        relations: ['specialities', 'places']
-      });
+        specialities: {
+          speciality_name: specialityName
+        }
+      },
+      relations: ['cities', 'specialities', 'services', 'places'],
+      skip: offset,
+      take: limit
+    });
 
-      return professionals;
-
-    }
-
-    async findAll(limit: number, offset: number): Promise<[Professional[], number]> {
-      const [results, total] = await this.professionalRepository.findAndCount({
-        skip: offset,
-        take: limit,
-        relations: ['cities', 'specialities', 'services']
-      });
-      return [results, total];
-    }
-
-
-    async findByCityAndSpeciality(cityName: string, specialityName: string, limit: number, offset: number): Promise<[Professional[], number]> {
-      const [results, total] = await this.professionalRepository
-        .createQueryBuilder('professional')
-        .leftJoinAndSelect('professional.cities', 'city')
-        .leftJoinAndSelect('professional.specialities', 'speciality')
-        .leftJoinAndSelect('professional.services', 'services')
-        .where('city.city_name = :cityName', { cityName })
-        .andWhere('speciality.speciality_name = :specialityName', { specialityName })
-        .skip(offset)
-        .take(limit)
-        .getManyAndCount();
-
-      return [results, total];
-    }
+    return [results, total];
+  }
 
   async findOne(id_professional: string) {
 
     let professional: Professional;
-    
+
     professional = await this.professionalRepository.findOne({
-        where: { id: id_professional },
-        relations: ['cities', 'specialities', 'services', 'questions', 'reviews'], // Include the cities relation here
+      where: { id: id_professional },
+      relations: ['cities', 'specialities', 'services', 'questions', 'reviews', 'places'], // Include the cities relation here
     });
-    
-    if(!professional){
+
+    if (!professional) {
       throw new NotFoundException(`Professional with ${id_professional} not found`)
     }
 
@@ -139,17 +142,17 @@ export class ProfessionalsService {
   async findOneNoRelationships(id_professional: string) {
 
 
-    if(!id_professional) {
+    if (!id_professional) {
       return null;
     }
 
     let professional: Professional;
-    
+
     professional = await this.professionalRepository.findOne({
-        where: { id: id_professional }
-      });
-    
-    if(!professional){
+      where: { id: id_professional }
+    });
+
+    if (!professional) {
       throw new NotFoundException(`Professional with ${id_professional} not found`)
     }
 
@@ -157,27 +160,27 @@ export class ProfessionalsService {
 
   }
 
-  async addServiceToProfessional(id_professional: string, id_service: string ) {
+  async addServiceToProfessional(id_professional: string, id_service: string) {
     let professional: Professional;
     let service: Service;
 
     professional = await this.findOne(id_professional);
-    if(!professional){
+    if (!professional) {
       throw new NotFoundException('professional not found')
     }
-    service =  await this.serviceService.findOne(id_service);
+    service = await this.serviceService.findOne(id_service);
 
-    if(!service){
+    if (!service) {
       throw new NotFoundException('services not found')
     }
-    
-    const services = await this.findServices(id_professional)
-    
 
-    if(!professional.services)
+    const services = await this.findServices(id_professional)
+
+
+    if (!professional.services)
       professional.services = []
 
-    
+
 
     professional.services.push(...services);
     professional.services.push(service)
@@ -186,20 +189,20 @@ export class ProfessionalsService {
     await this.professionalRepository.save(professional)
 
     return professional;
-    
+
   }
-  
-  async DeleteServiceToProfessional(id_professional:string, id_service:string){
+
+  async DeleteServiceToProfessional(id_professional: string, id_service: string) {
     let professional: Professional;
     let service: Service;
 
-    professional = await this.professionalRepository.findOne({where :{id: id_professional}, relations:['services']});
-    if(!professional){
+    professional = await this.professionalRepository.findOne({ where: { id: id_professional }, relations: ['services'] });
+    if (!professional) {
       throw new NotFoundException('professional not found')
     }
-    service =  await this.serviceService.findOne(id_service);
+    service = await this.serviceService.findOne(id_service);
 
-    if(!service){
+    if (!service) {
       throw new NotFoundException('services not found')
     }
 
@@ -209,17 +212,17 @@ export class ProfessionalsService {
 
   }
 
-  async DeleteSpecialityToProfessional(id_professional:string, id_speciality:string){
+  async DeleteSpecialityToProfessional(id_professional: string, id_speciality: string) {
     let professional: Professional;
     let speciality: Speciality;
 
-    professional = await this.professionalRepository.findOne({where :{id: id_professional}, relations:['specialities']});
-    if(!professional){
+    professional = await this.professionalRepository.findOne({ where: { id: id_professional }, relations: ['specialities'] });
+    if (!professional) {
       throw new NotFoundException('professional not found')
     }
-    speciality =  await this.specialityService.findOne(id_speciality);
+    speciality = await this.specialityService.findOne(id_speciality);
 
-    if(!speciality){
+    if (!speciality) {
       throw new NotFoundException('services not found')
     }
 
@@ -235,14 +238,14 @@ export class ProfessionalsService {
     let professional: Professional;
     let speciality: Speciality;
 
-    if(isUUID(id_professional) && isUUID(id_speciality)){
+    if (isUUID(id_professional) && isUUID(id_speciality)) {
       professional = await this.findOne(id_professional);
       speciality = await this.specialityService.findOne(id_speciality);
     }
 
     const specialities = await this.findSpecialities(id_professional)
 
-    if(!professional.specialities)
+    if (!professional.specialities)
       professional.specialities = []
 
     professional.specialities.push(...specialities)
@@ -257,13 +260,13 @@ export class ProfessionalsService {
     let professional: Professional;
     let city: City;
 
-    if(isUUID(id_professional) && isUUID(id_city)){
+    if (isUUID(id_professional) && isUUID(id_city)) {
       professional = await this.findOne(id_professional);
-      city = await this.CityRepository.findOneBy({id:id_city});
+      city = await this.CityRepository.findOneBy({ id: id_city });
     }
 
     const cities = await this.findCities(id_professional);
-    if(!professional.cities)
+    if (!professional.cities)
       professional.cities = []
 
     professional.cities.push(...cities);
@@ -276,29 +279,29 @@ export class ProfessionalsService {
 
 
 
-  async findServices(id_professional:string){
+  async findServices(id_professional: string) {
     return await this.serviceRepository.createQueryBuilder('service')
-        .innerJoin("service.professionals", "professional")
-        .where("professional.id = :id", {id: id_professional})
-        .getMany()
+      .innerJoin("service.professionals", "professional")
+      .where("professional.id = :id", { id: id_professional })
+      .getMany()
   }
 
-  async findSpecialities(id_professional: string){
+  async findSpecialities(id_professional: string) {
     return await this.specialityRepository.createQueryBuilder('speciality')
-        .innerJoin('speciality.professionals', 'professional')
-        .where('professional.id = :id', {id: id_professional})
-        .getMany()
+      .innerJoin('speciality.professionals', 'professional')
+      .where('professional.id = :id', { id: id_professional })
+      .getMany()
   }
 
-  async findAppoiments(id_professional:string) {
+  async findAppoiments(id_professional: string) {
 
     const professional = await this.findOne(id_professional);
-    
-    
+
+
     const appointments = this.AppoimentRepository.find({
       where: {
         professional: professional
-      }, 
+      },
       relations: ['client', 'professional', 'service']
     });
 
@@ -306,48 +309,48 @@ export class ProfessionalsService {
 
   }
 
-  async findByCity(city_name:string){
+  async findByCity(city_name: string) {
     return await this.professionalRepository
-        .createQueryBuilder('professional')
-        .innerJoin('professional.cities', 'city')
-        .where('city.city_name = :name', {name: city_name})
-        .getMany()
+      .createQueryBuilder('professional')
+      .innerJoin('professional.cities', 'city')
+      .where('city.city_name = :name', { name: city_name })
+      .getMany()
   }
 
-  async findCities(id_professional:string){
+  async findCities(id_professional: string) {
     return await this.CityRepository
-        .createQueryBuilder('city')
-        .innerJoin('city.professionals', 'professional')
-        .where('professional.id = :id', {id: id_professional})
-        .getMany();
+      .createQueryBuilder('city')
+      .innerJoin('city.professionals', 'professional')
+      .where('professional.id = :id', { id: id_professional })
+      .getMany();
   }
 
-  async findOneByEmail( email_professional: string ): Promise<Professional>{
+  async findOneByEmail(email_professional: string): Promise<Professional> {
     let professional: Professional;
-    professional = await this.professionalRepository.findOneBy({email: email_professional})
+    professional = await this.professionalRepository.findOneBy({ email: email_professional })
     return professional;
   }
 
   async update(id: string, updateProfessionalDto: UpdateProfessionalDto, professionalPhoto: Express.Multer.File) {
-  
+
     const professional = await this.professionalRepository.preload({
       id: id,
-      ... updateProfessionalDto
+      ...updateProfessionalDto
     });
 
-    if ( !professional ) throw new NotFoundException(`Professional with id: ${ id } not found`);
+    if (!professional) throw new NotFoundException(`Professional with id: ${id} not found`);
 
     try {
 
-      if(professionalPhoto){
+      if (professionalPhoto) {
         //Uploading the file to S3
         const photoUrl = await this.s3Service.uploadFile(professionalPhoto, professionalPhoto.originalname);
         professional.photo_url = photoUrl;
-     }
+      }
 
-      await this.professionalRepository.save( professional );
+      await this.professionalRepository.save(professional);
       return professional;
-      
+
     } catch (error) {
       this.handleDBExceptions(error);
     }
@@ -359,20 +362,20 @@ export class ProfessionalsService {
   }
 
 
-  private handleDBExceptions( error: any ) {
+  private handleDBExceptions(error: any) {
 
-    if ( error.code === '23505' )
+    if (error.code === '23505')
       throw new BadRequestException(error.detail);
-    
+
     this.logger.error(error)
     // console.log(error)
     throw new InternalServerErrorException('Unexpected error, check server logs');
 
   }
 
-  async getTotalReviews(professional_id:string){
+  async getTotalReviews(professional_id: string) {
     const professoinalReviews = await this.professionalRepository.findOne({
-      where: {id: professional_id},
+      where: { id: professional_id },
       relations: ['reviews']
     })
 
